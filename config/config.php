8e26030b0cf2a5868fd93f344c1789de200f1b10
mrmobi2006-source <mrmobi2006@gmail.com>
2026-05-06 08:50:28 +0100

    Set botToken, adminID, and logsID in config
    Updated botToken, adminID, and logsID in config.php.

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@
 | It can be obtained from https://telegram.dog/BotFather
 |
 */
-$config['botToken'] = "";
+$config['botToken'] = "8737011224:AAEJuLqYE-DZ1h96DR9jRktMcCDbP0yP9dQ";
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +20,7 @@ $config['botToken'] = "";
 | ID can be obtained from https://telegram.dog/username_to_id_bot
 |
 */
-$config['adminID'] = "";
+$config['adminID'] = "6154678499";
 
 /*
 |--------------------------------------------------------------------------
@@ -31,7 +31,7 @@ $config['adminID'] = "";
 | ID can be obtained from https://telegram.dog/BotFather
 |
 */
-$config['logsID'] =  "";
+$config['logsID'] =  "-1003973814680";
 
 /*
 |--------------------------------------------------------------------------
